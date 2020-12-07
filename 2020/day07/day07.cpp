a4dfbbbebea7493e83c477d8771d057bdf75dc48
hillsprig <hillsprig@gmail.com>
2020-12-07 02:32:04 +0100

    day05 & day06

---
+++ 2020/day07/day07.cpp
@@ -0,0 +1,29 @@
+#include <catch2/catch.hpp>
+
+#include "input.h"
+
+#include <algorithm>
+#include <array>
+#include <fmt/core.h>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <optional>
+#include <regex>
+#include <sstream>
+#include <vector>
+
+namespace {
+} // namespace
+
+TEST_CASE("day07_1_sample") {
+}
+
+TEST_CASE("day07_1_puzzle") {
+}
+
+TEST_CASE("day07_2_sample") {
+}
+
+TEST_CASE("day07_2_puzzle") {
+}
---
+++ 2020/day07/input.h
@@ -0,0 +1,4 @@
+#include <string>
+
+namespace input {
+} // namespace input
