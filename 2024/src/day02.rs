daf854a999df21b80192dd1591d067fd85c7bbf7
hillsprig <hillsprig@gmail.com>
2025-04-20 18:33:11 +0200

    Update

--- 2024/src/day02.rs
+++ 2024/src/day02.rs
@@ -1,5 +1,37 @@
 pub mod input;
 
 pub fn run() {
-    println!("{:?}", input::EXAMPLE);
+    println!("{:?}", safe(input::EXAMPLE));
+    println!("{:?}", safe(input::INPUT));
+}
+
+fn safe(input: &[&[i32]]) -> u32 {
+    let mut safe_reports: u32 = 0;
+    'outer: for report in input.iter() {
+        let increasing = report[0] - report[1] > 0;
+
+        for i in report.windows(2) {
+            if increasing && i[0] - i[1] > 0 {
+            } else {
+                continue 'outer;
+            }
+            if i[0].abs_diff(i[1]) >= 1 && i[0].abs_diff(i[1]) <= 3 {
+            } else {
+                continue 'outer;
+            }
+        }
+        safe_reports += 1;
+    }
+
+    safe_reports
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn problem1_example() {
+        assert_eq!(safe(input::EXAMPLE), 0);
+    }
 }
